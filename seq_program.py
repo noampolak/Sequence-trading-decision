05a6714ccfaff427812d4b03ae7e8be5e3f14aa8
noampolak <noampolak@gmail.com>
2019-11-18 22:52:09 +0200

    create main file and label_algo folder

---
+++ seq_program.py
@@ -0,0 +1,6 @@
+# loading all data files to memory as matrix
+# build and calc the label data - 1 for buy/sell for nothing
+# split the data to lots of timeframes - dev and test for fiting the model
+#fit the model
+#run the model on the test set
+#download updated data and test the model on them
