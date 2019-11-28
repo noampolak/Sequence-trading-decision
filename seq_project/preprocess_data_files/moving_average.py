e5b31bcdb0263ff26dbc52ffe120af6d4b249e94
noampolak <noampolak@gmail.com>
2019-11-29 00:12:04 +0200

    create moving average functions

---
+++ seq_project/preprocess_data_files/moving_average.py
@@ -0,0 +1,28 @@
+import pandas
+
+def rolling_mv(df_col,moving_average):
+    return df_col.rolling(moving_average).sum()
+def ewm_mv(df_col,moving_average):
+    return df_col.ewm(span=moving_average).mean()
+
+
+def add_moving_average_to_data_frame(df, moving_average, col_num, mv_type='moving_average'):
+    moving_average_types = {
+    'moving_average' : rolling_mv,
+    'ewm' : ewm_mv
+    }
+    func = moving_average_types.get(mv_type, lambda: "Invalid type")
+    mv_col = func(df.ix[:,col_num], moving_average)
+    df.insert(len(df.columns), "{}{}{}".format(col_num,mv_type,moving_average), mv_col)
+    return df
+
+def add_list_of_moving_average_to_data_frame(df, moving_average_list):
+    df_len = len(df.columns)
+    for col in range(1,df_len):
+        for moving_average in moving_average_list: 
+            print('col=', col)
+            print('moving_average=', moving_average)
+            df = add_moving_average_to_data_frame(df, moving_average, col, 'moving_average')
+            df = add_moving_average_to_data_frame(df, moving_average, col, 'ewm')
+
+    return df
--- seq_project/seq_program.py
+++ seq_project/seq_program.py
@@ -4,7 +4,7 @@
 #run the model on the test set
 #download updated data and test the model on them
 from settings import base_settings
-from preprocess_data_files import loadcsvfile
+from preprocess_data_files import loadcsvfile, moving_average
 
 # loading all data files to memory as matrix
 
@@ -12,3 +12,6 @@ files_df = loadcsvfile.load_all_csv_files_in_path(base_settings.HISTORY_INDEX_PA
 merged_data_frame = loadcsvfile.merge_all_data_frames_by_date_field(files_df)
 # first output - the merged data frame in csv file
 merged_data_frame.to_csv("{}/merged_data_frame.csv".format(base_settings.OUTPUTS_PATH))
+# create more fetures - moving averages and deltas
+moving_average_list = [3,5,10,20,30,50,100,200]
+merged_data_frame_plus_MA = moving_average.add_list_of_moving_average_to_data_frame(merged_data_frame, moving_average_list)
